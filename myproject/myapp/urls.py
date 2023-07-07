6a7894651306ef0e5aa75d813f3efcfb6183d9de
Mario Nikolic <mario13nikolic@gmail.com>
2023-07-07 22:55:01 +0200

    API

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from . import views
+from .views import *
 
 urlpatterns = [
     path('myview/', views.my_view, name='my_view'),
+    path('books/', BookListApiView.as_view()),
+    path('book/<int:book_id>/', BookDetailApiView.as_view())
+
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -7,3 +7,57 @@ from django.http import HttpResponse
 def my_view(request):
     # Add your view logic here
     return HttpResponse("This is my view!")
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import *
+from .serializers import *
+
+class BookListApiView(APIView):
+
+    # 1. List all
+    def get(self, request, *args, **kwargs):
+        
+        books = Book.objects.filter()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class BookDetailApiView(APIView):
+    def get_object(self,book_id):
+        try:
+            return Book.objects.get(id=book_id)        
+        except Book.DoesNotExist:
+            return None    
+
+    def get(self, request, book_id, *args, **kwargs):
+        book_instance = self.get_object(book_id)
+        if not book_instance:
+            return Response(
+                {"res": "Object with book id does not exist"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = BookSerializer(book_instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)  
+
+    '''
+    # 2. Create
+    def post(self, request, *args, **kwargs):
+        
+        data = {
+            'title': request.data.get('title'), 
+            'author': request.data.get('author'), 
+            'description': request.data.get('description'),
+            'publication_year':request.data.get('publication_year')
+        }
+        serializer = BookSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        '''
+        
